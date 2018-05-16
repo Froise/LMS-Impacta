d427a0dc9de4b829def1bd2e2e737004b08be515
Froise <31629809+Froise@users.noreply.github.com>
2018-05-16 01:58:51 -0300

    Update views.py

--- Projetos/LMS/contas/views.py
+++ Projetos/LMS/contas/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def listaAluno(request):
+    context{
+        "formAluno": Aluno.objects.all()
+    }
+    return render(request,"formNovoAluno.html",context)
+
+def listaProfessor(request):
+    context{
+        "formAluno": Professor.objects.all()
+    }
+    return render(request,"formNovoProfessor.html",context)
+
+def listaCoordenador(request):
+    context{
+        "formAluno": Coordenador.objects.all()
+    }
+    return render(request,"formNovoCoordenador.html",context)
